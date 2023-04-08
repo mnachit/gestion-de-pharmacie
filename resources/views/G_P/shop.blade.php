7338e6a7537e3fc18dc28f43aea61f7ec12bb5dc
philanthropist-mnachit <nachitmed70@gmail.com>
2023-04-08 21:18:23 +0000

    add5

--- resources/views/G_P/shop.blade.php
+++ resources/views/G_P/shop.blade.php
@@ -12,110 +12,18 @@
       </div>
     </div>
   </div>
-  
-{{-- <nav class="bg-white border-gray-200 dark:bg-gray-900">
-    <div class="flex flex-wrap items-center justify-between max-w-screen-xl mx-auto p-4">
-        <a href="https://flowbite.com" class="flex items-center">
-            <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
-            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-        </a>
-        <div class="flex items-center md:order-2">
-            <a href="#" class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 mr-1 md:mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Login</a>
-            <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 mr-1 md:mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Sign up</a>
-            <button data-collapse-toggle="mega-menu" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mega-menu" aria-expanded="false">
-                <span class="sr-only">Open main menu</span>
-                <svg aria-hidden="true" class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-            </button>
-        </div>
-        <div id="mega-menu" class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
-            <ul class="flex flex-col mt-4 font-medium md:flex-row md:space-x-8 md:mt-0">
-                <li>
-                    <a href="#" class="block py-2 pl-3 pr-4 text-blue-600 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-blue-500 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Home</a>
-                </li>
-                <li>
-                    <button id="mega-menu-dropdown-button" data-dropdown-toggle="mega-menu-dropdown" class="flex items-center justify-between w-full py-2 pl-3 pr-4 font-medium text-gray-900 border-b border-gray-100 md:w-auto hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">
-                        Company <svg aria-hidden="true" class="w-5 h-5 ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                    </button>
-                    <div id="mega-menu-dropdown" class="absolute z-10 grid hidden w-auto grid-cols-2 text-sm bg-white border border-gray-100 rounded-lg shadow-md dark:border-gray-700 md:grid-cols-3 dark:bg-gray-700">
-                        <div class="p-4 pb-0 text-gray-900 md:pb-4 dark:text-white">
-                            <ul class="space-y-4" aria-labelledby="mega-menu-dropdown-button">
-                                <li>
-                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
-                                        About Us
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
-                                        Library
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
-                                        Resources
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
-                                        Pro Version
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="p-4 pb-0 text-gray-900 md:pb-4 dark:text-white">
-                            <ul class="space-y-4">
-                                <li>
-                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
-                                        Blog
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
-                                        Newsletter
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
-                                        Playground
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
-                                        License
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="p-4">
-                            <ul class="space-y-4">
-                                <li>
-                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
-                                        Contact Us
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
-                                        Support Center
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
-                                        Terms
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </li>
-                <li>
-                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Team</a>
-                </li>
-                <li>
-                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
-                </li>
-            </ul>
+
+{{-- <div class="container">
+    <div class="row height d-flex justify-content-center align-items-center">
+        <div class="col-md-6">
+            <div class="form">
+                <i class="fa fa-search"></i>
+                <input type="text" class="form-control form-input" placeholder="Search anything..." id="searchbar" onkeyup="searche()">
+                <span class="left-pan"><i class="fa fa-microphone"></i></span>
+            </div>
         </div>
     </div>
-</nav> --}}
+</div> --}}
 
   <div class="site-section">
     <div class="container">
@@ -125,9 +33,16 @@
           <button type="button" class="btn btn-secondary btn-md dropdown-toggle px-4" id="dropdownMenuReference"
             data-toggle="dropdown"></button>
           <div class="dropdown-menu" aria-labelledby="dropdownMenuReference">
-            <a class="dropdown-item" href="#">Name, A to Z</a>
-            <a class="dropdown-item" href="#">Name, Z to A</a>
+            <a class="dropdown-item" href="#" onclick="Sort('az')">Name, A to Z</a>
+            <a class="dropdown-item" href="#" onclick="Sort('za')">Name, Z to A</a>
           </div>
+        </div>
+        <div class="col-md-6">
+            <div class="form">
+                <i class="fa fa-search"></i>
+                <input type="text" class="form-control form-input" placeholder="Search anything..." id="searchbar" onkeyup="searche()">
+                <span class="left-pan"><i class="fa fa-microphone"></i></span>
+            </div>
         </div>
       </div>
       
@@ -135,7 +50,7 @@
         @foreach ($Data_Shop as $show)
         <div class="col-sm-6 col-lg-4 text-center item mb-4">
           <a href="{{ route('shop-single',$show->id)}}"> <img src="{{ asset('img/blogs/'. $show->image)}}" alt="Image"></a>
-          <h3 class="text-dark"><a href="{{ route('shop-single',$show->id)}}">{{$show->Name}}</a></h3>
+          <h3 class="text-dark"><a href="{{ route('shop-single',$show->id)}}" id="titlee_">{{$show->Name}}</a></h3>
           <p class="price"><del>95.00</del> &mdash; $55.00</p>
         </div>
         @endforeach
@@ -164,51 +79,53 @@
       </div>
     </div>
   </div>
-
-
+  
 
   <script>
-    // get the dropdown menu element and the list of items to be sorted
-const dropdownMenu = document.querySelector('#dropdownMenuReference');
-const itemsList = document.querySelectorAll('.item');
+    function searche() {
+        let input = document.getElementById('searchbar').value.toLowerCase();
+        let items = document.getElementsByClassName('item');
 
-// add an event listener to the dropdown menu options
-dropdownMenu.addEventListener('click', function(event) {
-  // get the selected option
-  const selectedOption = event.target.textContent.trim();
-
-  // sort the items based on the selected option
-  if (selectedOption === 'Name, A to Z') {
-    sortItemsBy('asc');
-  } else if (selectedOption === 'Name, Z to A') {
-    sortItemsBy('desc');
-  }
-});
-
-// function to sort the items based on the given order (asc or desc)
-function sortItemsBy(order) {
-  // convert the items list to an array and sort it based on the name
-  const itemsArray = Array.from(itemsList);
-  itemsArray.sort(function(a, b) {
-    const nameA = a.querySelector('h3 a').textContent.trim();
-    const nameB = b.querySelector('h3 a').textContent.trim();
-    if (nameA < nameB) {
-      return order === 'asc' ? -1 : 1;
-    } else if (nameA > nameB) {
-      return order === 'asc' ? 1 : -1;
-    } else {
-      return 0;
+        for (let i = 0; i < items.length; i++) {
+            let title = items[i].querySelector('.text-dark a');
+            if (title.innerHTML.toLowerCase().includes(input)) {
+                items[i].style.display = 'block';
+            } else {
+                items[i].style.display = 'none';
+            }
+        }
     }
-  });
 
-  // remove the existing items from the DOM and append the sorted items
-  itemsList.forEach(function(item) {
-    item.remove();
-  });
-  itemsArray.forEach(function(item) {
-    document.querySelector('.row').appendChild(item);
-  });
-}
+    function Sort(sortOrder) {
+        // console.log('oui');
+        // return
+        let data = []
+        let items = document.getElementsByClassName('item');
+        console.log(items);
+        for (let i = 0; i < items.length; i++) {
+            let title = items[i].querySelector('.text-dark a');
+            data.push(title.textContent)
+        }
 
+        // Sort the data based on sortOrder (asc/desc)
+        if (sortOrder === 'az') {
+        data.sort();
+        } else if (sortOrder === 'za') {
+        data.sort().reverse();
+        }
+
+        // Loop through the sorted data and update the DOM
+        let container = document.getElementById('shop-container');
+        for (let i = 0; i < data.length; i++) {
+        let item = container.querySelector('.item:nth-of-type(' + (i + 1) + ')');
+        let title = item.querySelector('.text-dark a');
+        if (title.textContent === data[i]) {
+            continue; // Skip if the item is already in the correct position
+        }
+        // Move the item to its new position in the DOM
+        container.insertBefore(item, container.querySelector('.item:nth-of-type(' + (data.indexOf(title.textContent) + 1) + ')'));
+        }
+
+    }
   </script>
 @endsection
